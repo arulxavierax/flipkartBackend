637d4de25c33d35a8e3ed4f12f414d33cd438c68
arul xavier <arulxavier2121@gmail.com>
2022-12-10 18:26:50 +0530

    backend

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const dbConnect = require("./config/db");
 const productsRoute = require("./features/products/products.router");
 const userRoute = require("./features/users/user.router");
 const resetpasswordRouter = require("./features/reset-password/resetPassword.router");
+const dotenv = require("dotenv");
 
 const app = express();
 app.use(cors());
+dotenv.config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
