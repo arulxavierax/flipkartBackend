5364f63866e5a59a581e4cd2b4f344ae9fdf73ab
arul xavier <arulxavier2121@gmail.com>
2023-02-28 11:54:37 +0530

    cart page changes added

--- features/cart/cart.router.js
+++ features/cart/cart.router.js
@@ -3,41 +3,14 @@ const Cart = require("./cart.model");
 const Products = require("../products/products.model");
 const jwt = require("jsonwebtoken");
 
-// const authMiddleware = async (req, res, next) => {
-//   let token = req.headers.token;
-//   if (token) {
-//     try {
-//       let data = jwt.verify(token, process.env.SECRETKEY);
-//       if (data.id) {
-//         req.userId = data.id;
-//         next();
-//       }
-//     } catch (e) {
-//       res.status(500).send(e.message);
-//     }
-//   } else {
-//     res.status(500).send("Something went wrong");
-//   }
-// };
-
-const app = express.Router();
-// app.use(authMiddleware);
-
-app.get("/", async (req, res) => {
+const authMiddleware = async (req, res, next) => {
   let token = req.headers.token;
   if (token) {
     try {
       let data = jwt.verify(token, process.env.SECRETKEY);
       if (data.id) {
-        let items = await Cart.find({ userId: data.id });
-        let cartitems = [];
-        for (var i = 0; i < items.length; i++) {
-          cartitems.push({
-            product: await Products.findById(items[i].product),
-            quantity: items[i].quantity,
-          });
-        }
-        res.send(cartitems);
+        req.userId = data.id;
+        next();
       }
     } catch (e) {
       res.status(500).send(e.message);
@@ -45,57 +18,52 @@ app.get("/", async (req, res) => {
   } else {
     res.status(500).send("Something went wrong");
   }
+};
+
+const app = express.Router();
+app.use(authMiddleware);
+
+app.get("/", async (req, res) => {
+  let items = await Cart.find({ userId: req.userId });
+  let cartitems = [];
+  for (var i = 0; i < items.length; i++) {
+    cartitems.push({
+      product: await Products.findById(items[i].product),
+      quantity: items[i].quantity,
+    });
+  }
+  res.send(cartitems);
 });
 
 app.post("/", async (req, res) => {
-  let token = req.headers.token;
-  if (token) {
-    try {
-      let data = jwt.verify(token, process.env.SECRETKEY);
-      if (data.id) {
-        let existingUserItem = await Cart.findOne({
-          user: data.id,
-          product: req.body.product,
-        });
-        if (existingUserItem) {
-          let item = await Cart.findByIdAndUpdate(existingUserItem._id, {
-            quantity: req.body.quantity,
-          });
-          return res.send("Product Updated");
-        } else {
-          let item = await Cart.create({
-            ...req.body,
-            user: data.id,
-          });
-          res.send(item);
-        }
-      }
-    } catch (e) {
-      res.status(500).send(e.message);
+  try {
+    let existingUserItem = await Cart.findOne({
+      user: req.userId,
+      product: req.body.product,
+    });
+    if (existingUserItem) {
+      let item = await Cart.findByIdAndUpdate(existingUserItem._id, {
+        quantity: req.body.quantity,
+      });
+      return res.send("Product Updated");
+    } else {
+      let item = await Cart.create({
+        ...req.body,
+        user: req.userId,
+      });
+      res.send(item);
     }
-  } else {
-    res.status(500).send("Something went wrong");
+  } catch (e) {
+    return res.status(500).send(e.message);
   }
 });
 
 app.delete("/delete", async (req, res) => {
-  let token = req.headers.token;
-  if (token) {
-    try {
-      let data = jwt.verify(token, process.env.SECRETKEY);
-      if (data.id) {
-        let existingProduct = await Cart.findOneAndDelete({
-          user: data.id,
-          product: req.body.product,
-        });
-        res.send("Product removed sucessfully");
-      }
-    } catch (e) {
-      res.status(500).send(e.message);
-    }
-  } else {
-    res.status(500).send("Something went wrong");
-  }
+  let existingProduct = await Cart.findOneAndDelete({
+    user: req.userId,
+    product: req.body.product,
+  });
+  res.send("Product removed sucessfully");
 });
 
 module.exports = app;
